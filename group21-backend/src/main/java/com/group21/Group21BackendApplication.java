4c22f3fbf2ca0dc4e809a2f5b0dd51cea7ca4ac8
Rosana-Todosieva <93923104+Rosana-Todosieva@users.noreply.github.com>
2022-04-18 19:34:05 +0200

    Add files via upload

---
+++ group21-backend/src/main/java/com/group21/Group21BackendApplication.java
@@ -0,0 +1,13 @@
+package com.group21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group21BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Group21BackendApplication.class, args);
+	}
+
+}
