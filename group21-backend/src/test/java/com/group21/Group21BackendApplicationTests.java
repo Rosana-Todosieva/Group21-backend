6867f940dc4caee3b5e31d8f1cdd9b1f1370a007
Rosana-Todosieva <93923104+Rosana-Todosieva@users.noreply.github.com>
2022-04-18 19:41:06 +0200

    Add files via upload

---
+++ group21-backend/src/test/java/com/group21/Group21BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group21BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
